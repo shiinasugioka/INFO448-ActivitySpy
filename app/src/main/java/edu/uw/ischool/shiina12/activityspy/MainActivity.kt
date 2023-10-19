37b35005f5ef83c2ee302f401e4b64fd59f121fb
Shiina Sugioka <shiina12@uw.edu>
2023-10-19 13:14:24 -0700

    added changes to main activity

--- app/src/main/java/edu/uw/ischool/shiina12/activityspy/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/shiina12/activityspy/MainActivity.kt
@@ -1,11 +1,41 @@
 package edu.uw.ischool.shiina12.activityspy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i(TAG, "onCreate event fired with params: $savedInstanceState")
+    }
+
+    public override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy event fired")
+        Log.e(TAG, "We're going down, Captain!")
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart event fired")
+    }
+
+    public override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop event fired")
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause event fired")
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume event fired")
     }
 }
